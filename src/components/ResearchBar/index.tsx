b2acf47da78cf5a9e7bd55b831066961ce7b50dc
NoPYXN <yunfournes@gmail.com>
2023-11-28 00:08:47 +0100

    Tentative de mettre le composant ResearchBar

--- src/components/ResearchBar/index.tsx
+++ src/components/ResearchBar/index.tsx
@@ -1,79 +1,67 @@
-import React, { useState, useEffect } from 'react';
-import Autosuggest from 'react-autosuggest';
+import React, { useState, useEffect, useMemo } from "react"
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
+import usePlacesAutocomplete, { getGeocode, getLatLng } from "use-places-autocomplete"
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+} from "@reach/combobox"
+import "@reach/combobox/styles.css"
 
-interface Location {
-  name: string;
+export default function Index() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: "AIzaSyDbr6FgqPsctO5kXmIFoYL7X7TuaXAGX_o",
+        libraries: ["places"],
+    })
+
+    if (!isLoaded) return <div>Loading...</div>
+    return <Map />
+}
+function Map() {
+    const [selected, setSelected] = useState(null)
+    return (
+        <div>
+            <div>
+                <PlacesAutocomplete selected={selected} setSelected={setSelected} />
+            </div>
+        </div>
+    )
 }
 
-interface SearchBarProps {
-  onCitySelected: (city: string) => void;
-}
+const PlacesAutocomplete = (props: { selected: any; setSelected: any }) => {
+    const {
+        ready,
+        value,
+        setValue,
+        suggestions: { status, data },
+        clearSuggestions,
+    } = usePlacesAutocomplete()
 
-const SearchBar: React.FC<SearchBarProps> = ({ onCitySelected }) => {
-  const [value, setValue] = useState<string>('');
-  const [suggestions, setSuggestions] = useState<Location[]>([]);
-
-  const fetchSuggestions = async (inputValue: string) => {
-    try {
-      const response = await fetch(
-        `http://api.geonames.org/searchJSON?name_startsWith=${inputValue}&country=FR&username=YOUR_USERNAME`
-      );
-
-      if (!response.ok) {
-        throw new Error('Unable to fetch suggestions');
-      }
-
-      const data = await response.json();
-      setSuggestions(data.geonames);
-    } catch (error) {
-      console.error('Error fetching suggestions:', error);
+    const handleSelect = async (address: any) => {
+        setValue(address, false)
+        clearSuggestions()
+        const results = await getGeocode({ address })
+        const { lat, lng } = getLatLng(results[0])
+        props.setSelected({ lat, lng })
     }
-  };
-
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>, { newValue }: { newValue: string }) => {
-    setValue(newValue);
-  };
-
-  const onSuggestionsFetchRequested = ({ value }: { value: string }) => {
-    fetchSuggestions(value);
-  };
-
-  const onSuggestionsClearRequested = () => {
-    setSuggestions([]);
-  };
-
-  const onSuggestionSelected = (
-    event: React.FormEvent<HTMLInputElement>,
-    { suggestion }: { suggestion: Location }
-  ) => {
-    onCitySelected(suggestion.name);
-  };
-
-  const getSuggestionValue = (suggestion: Location) => suggestion.name;
-
-  const renderSuggestion = (suggestion: Location) => <div>{suggestion.name}</div>;
-
-  const inputProps = {
-    placeholder: 'Rechercher une ville en France',
-    value,
-    onChange,
-  };
-
-  useEffect(() => {
-    // Ajoutez ici d'autres logiques liées à la recherche, si nécessaire
-  }, [value]);
-
-  return (
-    <Autosuggest
-      suggestions={suggestions}
-      onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-      onSuggestionsClearRequested={onSuggestionsClearRequested}
-      onSuggestionSelected={onSuggestionSelected}
-      getSuggestionValue={getSuggestionValue}
-      renderSuggestion={renderSuggestion}
-      inputProps={inputProps}
-    />
-  );
-};
-
-export default SearchBar;
+    return (
+        <Combobox onSelect={handleSelect}>
+            <ComboboxInput
+                value={value}
+                onChange={e => setValue(e.target.value)}
+                disabled={!ready}
+                placeholder="Search an adress"
+            />
+            <ComboboxPopover>
+                <ComboboxList>
+                    {status === "OK" &&
+                        data.map(({ place_id, description }) => (
+                            <ComboboxOption key={place_id} value={description} />
+                        ))}
+                </ComboboxList>
+            </ComboboxPopover>
+        </Combobox>
+    )
+}
