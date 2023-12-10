35c140de173189aca522fe49c14a80ea8ef7939f
Melly Lucas <melly.lucas32@yahoo.fr>
2023-12-10 16:32:50 +0100

    git merge

--- src/components/researchBar/index.tsx
+++ src/components/researchBar/index.tsx
@@ -10,42 +10,54 @@ import {
 } from "@reach/combobox"
 import "@reach/combobox/styles.css"
 
-export default function Index() {
-    const { isLoaded } = useLoadScript({
-        googleMapsApiKey: "AIzaSyDbr6FgqPsctO5kXmIFoYL7X7TuaXAGX_o",
-        libraries: ["places"],
-    })
-
-    if (!isLoaded) return <div>Loading...</div>
-    return <Map />
+export default function Index(props: { setSearchVille: any, isLoaded: any }) {
+    // const { isLoaded } = useLoadScript({
+    //     // googleMapsApiKey: "AIzaSyDbr6FgqPsctO5kXmIFoYL7X7TuaXAGX_o",
+    //     libraries: ["places"],
+    // })
+    console.log(props.isLoaded, "isloadee")
+    if (!props.isLoaded) return <div>Loading...</div>
+    return <Map setSearchVille={props.setSearchVille} />
 }
-function Map() {
-    const [selected, setSelected] = useState(null)
+function Map(props: { setSearchVille: any }) {
+    const [selected, setSelected] = useState<any>("")
     return (
         <div>
             <div>
-                <PlacesAutocomplete selected={selected} setSelected={setSelected} />
+                <PlacesAutocomplete setSearchVille={props.setSearchVille} selected={selected} setSelected={setSelected} />
             </div>
         </div>
     )
 }
 
-const PlacesAutocomplete = (props: { selected: any; setSelected: any }) => {
+const PlacesAutocomplete = (props: { selected: any; setSelected: any, setSearchVille: any }) => {
     const {
         ready,
         value,
         setValue,
         suggestions: { status, data },
         clearSuggestions,
-    } = usePlacesAutocomplete()
+    } = usePlacesAutocomplete({
+        requestOptions: {
+            types: ['geocode'],
+            // 
+            // componentRestrictions: {
+            //     country: 'fr',
+            // },
+            // types: ['(cities)', '(fr)'],
+        }
+    })
 
     const handleSelect = async (address: any) => {
         setValue(address, false)
         clearSuggestions()
         const results = await getGeocode({ address })
         const { lat, lng } = getLatLng(results[0])
-        props.setSelected({ lat, lng })
+        // props.setSelected({ lat, lng })
+        props.setSearchVille(address.split(",")[0])
+
     }
+
     return (
         <Combobox onSelect={handleSelect}>
             <ComboboxInput
@@ -57,9 +69,18 @@ const PlacesAutocomplete = (props: { selected: any; setSelected: any }) => {
             <ComboboxPopover>
                 <ComboboxList>
                     {status === "OK" &&
-                        data.map(({ place_id, description }) => (
-                            <ComboboxOption key={place_id} value={description} />
-                        ))}
+                        data
+                            .filter(({ types }) =>
+                                types.includes("locality")
+                            )
+
+                            .filter(({ terms }) =>
+                                terms.some(obj => obj.value === 'France') == true
+                            )
+
+                            .map(({ place_id, description }) => (
+                                <ComboboxOption key={place_id} value={description} />
+                            ))}
                 </ComboboxList>
             </ComboboxPopover>
         </Combobox>
